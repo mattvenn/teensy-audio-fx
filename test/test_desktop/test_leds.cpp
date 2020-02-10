b4fc27dfa06a589e83f1fc10050a3d3afb58fa92
matt venn <matt@mattvenn.net>
2020-02-10 12:21:49 +0100

    updates for extra control arg and sub millisecond bar timer

--- test/test_desktop/test_leds.cpp
+++ test/test_desktop/test_leds.cpp
@@ -15,22 +15,23 @@ Button buttons[NUM_BUTTONS] = { Button(0), Button(1) }; //(0, 1, 17, 22);
 BarTimer bar_timer;
 
 void test_bar_timer(void){
+
     bar_timer.set_bpm(120);
     bar_timer.update(true);
-    // 120bpm with 64 steps per bar is 125ms between beats
-    TEST_ASSERT_EQUAL(bar_timer.get_next_step_millis(), 125);
+    // 120bpm with 512 steps per bar is 15.625 between beats
+    TEST_ASSERT_EQUAL(bar_timer.get_next_step_millis(), 15);
     TEST_ASSERT_EQUAL(bar_timer.get_step(), 0);
-    for(int ms = 0; ms < 125 * 64; ms ++) {
+    for(int ms = 0; ms < 15.625 * MAX_STEPS-1; ms ++) {
         bar_timer.set_millis(ms);
         bar_timer.update(false);
-//        printf("ms = %d ms/125 = %d, step = %d\n", ms, ms/125, bar_timer.get_step());
-        TEST_ASSERT_EQUAL(bar_timer.get_step(), ms/125);
+        printf("ms = %d ms/15.625 = %f, step = %d\n", ms, ms/15.625, bar_timer.get_step());
+        TEST_ASSERT_INT_WITHIN(1, bar_timer.get_step(), ms/15.625); //.625);expected, actual)
     }
-    // rollover
-    int ms = 125 * 65;
+
+    // test rollover
+    int ms = MAX_STEPS * 15.625;
     bar_timer.set_millis(ms);
     bar_timer.update(false);
-//    printf("ms = %d ms/125 = %d, step = %d\n", ms, ms/125, bar_timer.get_step());
     TEST_ASSERT_EQUAL(bar_timer.get_step(), 0);
 }
 void test_button(void) {
@@ -40,7 +41,7 @@ void test_button(void) {
 void test_control_led_and_param(void) {
     for(int i = 0; i < 1024; i += 200)
     {
-        control[0].set_val(i,0,true,false);
+        control[0].set_val(i,0,true, false, false);
         printf("pot %d val %.2f led %d\n", i, control[0].get_val(0), control[0].get_led_val(0));
     }
 
@@ -48,7 +49,7 @@ void test_control_led_and_param(void) {
 void test_control(void) {
     for(int i = 0; i < NUM_CONTROLS; i ++)
     {
-        control[i].set_val(500,0,true,false);
+        control[i].set_val(500,0,true,false, false);
         TEST_ASSERT_EQUAL(float(500)/1024, control[i].get_val(0));
         TEST_ASSERT_EQUAL(500/(1024/256), control[i].get_led_val(0));
     }
@@ -57,14 +58,14 @@ void test_control(void) {
 void test_control_seq(void) {
     // write
     for(int i = 0; i < MAX_STEPS; i ++)
-        control[0].set_val(i, i, true, true);
+        control[0].set_val(i, i, true, true, false);
 
     // read
     for(int i = 0; i < MAX_STEPS; i ++)
         TEST_ASSERT_EQUAL(float(i)/1024, control[0].get_val(i));
 
     // set to single value
-    control[0].set_val(0, 0, true, false);
+    control[0].set_val(0, 0, true, false, false);
 
     // read
     for(int i = 0; i < MAX_STEPS; i ++)
